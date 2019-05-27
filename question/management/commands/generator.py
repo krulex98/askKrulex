d44b07135c29bafc8cf2cbbb2eadbf6124f7d39f
krulex98 <krulex98@gmail.com>
2019-05-27 16:07:00 +0300

    models question, answer, tag + managements commands for generate

--- question/management/commands/generator.py
+++ question/management/commands/generator.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
+from random import choice, randint
 
-from question.models import User
+from question.models import *
 
 
 def debug_print(iter, total, prefix='', suffix='', decimals=1, len=100, fill=' '):
@@ -21,7 +22,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         items_count = options['items_count']
-        self.generate_users(items_count)
+        self.generate(items_count)
 
     @classmethod
     def generate_users(cls, count):
@@ -30,6 +31,10 @@ class Command(BaseCommand):
 
         for i in range(count):
             fake_profile = cls.fake.simple_profile(sex=None)
+            username = fake_profile['username']
+            while any(user.username == username for user in users):
+                fake_profile = cls.fake.simple_profile(sex=None)
+                username = fake_profile['username']
             name = fake_profile['name'].split()
             first_name = name[0]
             last_name = name[1]
@@ -47,3 +52,90 @@ class Command(BaseCommand):
 
         debug_print(count, count, prefix='Generate users:', suffix='Complete', len=50)
         return users
+
+    @classmethod
+    def generate_questions(cls, count, users, tags):
+        questions = []
+        debug_print(0, count, prefix='Generate questions:', suffix='Complete', len=50)
+
+        for i in range(count):
+            question = Question.objects.create(
+                author=choice(users),
+                title=cls.fake.sentence(),
+                text=cls.fake.text(max_nb_chars=1280)
+            )
+            for _ in range(randint(2, 6)):
+                question.tags.add(choice(tags))
+            question.save()
+            questions.append(question)
+            debug_print(i, count, prefix='Generate questions:', suffix='Complete', len=50)
+
+        debug_print(count, count, prefix='Generate questions:', suffix='Complete', len=50)
+        return questions
+
+
+    @classmethod
+    def generate_answers(cls, count, questions, users):
+        answers = []
+        debug_print(0, count, prefix='Generate answers:', suffix='Complete', len=50)
+        j = 0
+
+        for question in questions:
+
+            for i in range(randint(1, 10)):
+                answer = Answer.objects.create(
+                    text=cls.fake.text(max_nb_chars=200),
+                    author=choice(users),
+                    quest=question
+                )
+                answer.save()
+                answers.append(answer)
+            debug_print(j, count, prefix='Generate answers:', suffix='Complete', len=50)
+            j += 1
+
+        debug_print(count, count, prefix='Generate answers:', suffix='Complete', len=50)
+        return answers
+
+
+    @classmethod
+    def generate_tags(cls, count):
+        tags = []
+        debug_print(0, count, prefix='Generate tags:', suffix='Complete', len=50)
+
+        for i in range(count):
+            tag = Tag.objects.create(title=cls.fake.word())
+            tag.save()
+            tags.append(tag)
+            debug_print(i, count, prefix='Generate tags:', suffix='Complete', len=50)
+
+        debug_print(count, count, prefix='Generate tags:', suffix='Complete', len=50)
+        return tags
+
+    @classmethod
+    def generate_marks(cls, count, like_able, users):
+        debug_print(0, count, prefix='Generate marks:', suffix='Complete', len=50)
+
+        for i in range(count):
+            if choice([True, False]):
+                choice(like_able).like(choice(users))
+            else:
+                choice(like_able).dislike(choice(users))
+            debug_print(i, count, prefix='Generate marks:', suffix='Complete', len=50)
+
+        debug_print(count, count, prefix='Generate marks:', suffix='Complete', len=50)
+
+    @classmethod
+    def generate(cls, count):
+        users = cls.generate_users(count)
+
+        tags = cls.generate_tags(count)
+
+        que_count = count * 10
+        questions = cls.generate_questions(que_count, users, tags)
+
+        answer_count = count * 100
+        answers = cls.generate_answers(answer_count, questions, users)
+
+        marks_count = count * 100
+        cls.generate_marks(marks_count, questions, users)
+        cls.generate_marks(marks_count, answers, users)
--- question/models.py
+++ question/models.py
@@ -1,10 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=64, null=False)
     avatar = models.ImageField(upload_to='static/media/images/user-avatar', default='static/img/default-avatar.jpg')
 
+
+class LikeAbleModel(models.Model):
+    likes = models.ManyToManyField(User, related_name='likes')
+    dislikes = models.ManyToManyField(User, related_name='dislikes')
+
+    def like(self, user):
+        self.dislikes.remove(user)
+        self.likes.add(user)
+
+    def dislike(self, user):
+        self.likes.remove(user)
+        self.dislikes.add(user)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=32, null=False)
+
+
+class Question(LikeAbleModel):
+    title = models.CharField(max_length=128, null=False)
+    text = models.CharField(max_length=2560, null=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+    date = models.DateField(auto_now=True)
+
+
+class Answer(LikeAbleModel):
+    quest = models.ForeignKey(Question,  on_delete=models.CASCADE, related_name='question')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='author')
+    text = models.TextField(max_length=2560)
+    date = models.DateField(auto_now=True)
