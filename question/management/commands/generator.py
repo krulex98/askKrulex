31033dc3469685d39a51c69687e5c46b4fcab0a0
krulex98 <krulex98@gmail.com>
2019-05-27 10:11:59 +0300

    managements commands: generate users

--- question/management/commands/generator.py
+++ question/management/commands/generator.py
@@ -1,18 +1,49 @@
 from django.core.management.base import BaseCommand
+from faker import Faker
+
+from question.models import User
+
+
+def debug_print(iter, total, prefix='', suffix='', decimals=1, len=100, fill=' '):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iter / float(total)))
+    filled_length = int(len * iter // total)
+    bar = fill * filled_length + '-' * (len - filled_length)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    if iter == total:
+        print()
 
 class Command(BaseCommand):
 
-    def debug_print(self):
-        pass
+    fake = Faker()
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('items_count', type=int)
 
     def handle(self, *args, **options):
-        pass
+        items_count = options['items_count']
+        self.generate_users(items_count)
 
-    def generate_users(self):
-        pass
+    @classmethod
+    def generate_users(cls, count):
+        users = []
+        debug_print(0, count, prefix='Generate users:', suffix='Complete', len=50)
 
-    def generate_quests(self):
-        pass
+        for i in range(count):
+            fake_profile = cls.fake.simple_profile(sex=None)
+            name = fake_profile['name'].split()
+            first_name = name[0]
+            last_name = name[1]
+            user = User.objects.create_user(
+                username=fake_profile['username'],
+                nickname=fake_profile['username'],
+                first_name=first_name,
+                last_name=last_name,
+                email=fake_profile['mail'],
+                password=fake_profile['username']
+            )
+            user.save()
+            users.append(user)
+            debug_print(i, count, prefix='Generate users:', suffix='Complete', len=50)
+
+        debug_print(count, count, prefix='Generate users:', suffix='Complete', len=50)
+        return users
